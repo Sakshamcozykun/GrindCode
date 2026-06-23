04fddd7b7d514280646eae0b54c6f88fe17f0855
Saksham Gupta <sakshamofficialwork@gmail.com>
2026-06-23 16:09:57 +0530

    Add Rotate Image [C++]

---
+++ medium/rotate-image.cpp
@@ -0,0 +1,23 @@
+// Problem : Rotate Image
+// Slug    : https://leetcode.com/problems/rotate-image/
+// Lang    : C++
+// Difficulty: Medium
+// Synced  : 2026-06-23T10:39:54.091Z
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int N = matrix.size();
+        
+        //find transpose
+        for(int i = 0; i<N; i++) {
+            for(int j = i; j<N; j++) {
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        
+        //flip horizontally - For clock wise rotation
+        for(int i = 0; i<N; i++) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    };
+};
