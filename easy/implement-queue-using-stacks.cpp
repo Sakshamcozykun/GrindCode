ae301b1885f1abc8f804b015ccefd2472964e9a8
Saksham Gupta <sakshamofficialwork@gmail.com>
2026-06-24 18:34:17 +0530

    Add Implement Queue using Stacks [C++]

--- easy/implement-queue-using-stacks.cpp
+++ easy/implement-queue-using-stacks.cpp
@@ -2,22 +2,50 @@
 // Slug    : https://leetcode.com/problems/implement-queue-using-stacks/
 // Lang    : C++
 // Difficulty: Easy
-// Synced  : 2026-06-24T12:52:42.981Z
-    }
-    
-    int peek() {
-        if(output.empty())
-            return peekEl;
-        
-        return output.top();
-    }
-    
-    bool empty() {
-        return val;
-        output.pop();
-        if(input.empty() && output.empty())
-            return true;
-        
-        return false;
-    }
+// Synced  : 2026-06-24T13:04:13.931Z
+class MyQueue {
+public:
+    
+    stack<int> input;
+    stack<int> output;
+    int peekEl = -1;
+    
+    MyQueue() {
+        
+    }
+    
+    void push(int x) {
+        if(input.empty()) {
+            peekEl = x;
+        }
+        input.push(x); 
+    }
+    //amortized O(1)
+    int pop() {
+        if(output.empty()) {
+           
+            while(!input.empty()) {
+                output.push(input.top());
+                input.pop();
+            }
+        }
+        
+        int val = output.top(); //O(1)
+        output.pop();
+        return val;
+    }
+    
+    int peek() {
+        if(output.empty())
+            return peekEl;
+        
+        return output.top();
+    }
+    
+    bool empty() {
+        if(input.empty() && output.empty())
+            return true;
+        
+        return false;
+    }
 };
