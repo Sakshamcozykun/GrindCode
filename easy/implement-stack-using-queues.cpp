2a4c6e01181591480d726347d8b927578673be63
Saksham Gupta <sakshamofficialwork@gmail.com>
2026-06-24 18:04:09 +0530

    Add Implement Stack using Queues [C++]

--- easy/implement-stack-using-queues.cpp
+++ easy/implement-stack-using-queues.cpp
@@ -2,24 +2,20 @@
 // Slug    : https://leetcode.com/problems/implement-stack-using-queues/
 // Lang    : C++
 // Difficulty: Easy
-// Synced  : 2026-06-24T05:43:40.171Z
-    }
-    
-    /** Removes the element on top of the stack and returns that element. */
+// Synced  : 2026-06-24T12:34:05.349Z
     int pop() {
-        swap(q1, q2);
-        while(!q1.empty()){
-            q2.push(q1.front());    q1.pop();
-        }
-    void push(int x) {
-        q2.push(x);
-    
-    /** Push element x onto stack. */
-    MyStack() {
-        
+        int x = que.front();
+        que.pop();
+        return x;
     }
-    queue<int> q2;
-    queue<int> q1;
-    /** Initialize your data structure here. */
-public:
-class MyStack {
+    
+ 
+    int top() {
+        return que.front();
+    }
+    
+    
+    bool empty() {
+        return que.empty();
+    }
+};
