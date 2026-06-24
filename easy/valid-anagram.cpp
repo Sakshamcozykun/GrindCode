60955f626dcf1fe9d94d9ef86f274e84b2ade286
Saksham Gupta <sakshamofficialwork@gmail.com>
2026-06-25 00:52:46 +0530

    Add Valid Anagram [C++]

--- easy/valid-anagram.cpp
+++ easy/valid-anagram.cpp
@@ -2,13 +2,24 @@
 // Slug    : https://leetcode.com/problems/valid-anagram/
 // Lang    : C++
 // Difficulty: Easy
-// Synced  : 2026-06-24T19:22:06.629Z
+// Synced  : 2026-06-24T19:22:43.517Z
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(begin(s), end(s));
-        sort(begin(t), end(t));
+        vector<int> count(26, 0);
         
-        return s == t;
+        for(char &ch : s) {
+            count[ch-'a']++;
+        }
+        
+        for(char &ch : t) {
+            count[ch-'a']--;
+        }
+        
+        bool allZeros = all_of(begin(count), end(count), [](int element) {
+            return element == 0;
+        });
+        
+        return allZeros;
     }
 };
