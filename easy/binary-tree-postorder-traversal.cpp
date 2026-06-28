b572599e2ef4d696688627a4730ecac9358e3849
Saksham Gupta <sakshamofficialwork@gmail.com>
2026-06-28 13:47:55 +0530

    Add Binary Tree Postorder Traversal [C++]

---
+++ easy/binary-tree-postorder-traversal.cpp
@@ -0,0 +1,50 @@
+// Problem : Binary Tree Postorder Traversal
+// Slug    : https://leetcode.com/problems/binary-tree-postorder-traversal/
+// Lang    : C++
+// Difficulty: Easy
+// Synced  : 2026-06-28T08:17:50.606Z
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        
+        if (root == nullptr)
+            return ans;
+
+        stack<TreeNode*> s1, s2;
+
+        s1.push(root);
+
+        while (!s1.empty()) {
+            TreeNode* curr = s1.top();
+            s1.pop();
+
+            s2.push(curr);
+
+            if (curr->left)
+                s1.push(curr->left);
+
+            if (curr->right)
+                s1.push(curr->right);
+        }
+
+        while (!s2.empty()) {
+            ans.push_back(s2.top()->val);
+            s2.pop();
+        }
+
+        return ans;
+    }
+};
