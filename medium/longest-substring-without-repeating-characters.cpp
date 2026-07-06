4957c22bce0b5beff60b09f8022fe9ee320d1b6a
Saksham Gupta <sakshamofficialwork@gmail.com>
2026-07-06 12:50:45 +0530

    Add Longest Substring Without Repeating Characters [C++]

--- medium/longest-substring-without-repeating-characters.cpp
+++ medium/longest-substring-without-repeating-characters.cpp
@@ -2,27 +2,27 @@
 // Slug    : https://leetcode.com/problems/longest-substring-without-repeating-characters/
 // Lang    : C++
 // Difficulty: Medium
-// Runtime : 3 ms
-// Memory  : 11.7 MB
-// Synced  : 2026-07-06T07:18:17.781Z
+// Runtime : 572 ms
+// Memory  : 153.7 MB
+// Synced  : 2026-07-06T07:20:40.487Z
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         unordered_set<char> st;
-        int n = s.size();
+
+        int left = 0;
         int ans = 0;
 
-        for (int i = 0; i < n; i++) {
-            st.clear();
+        for (int right = 0; right < s.size(); right++) {
 
-            for (int j = i; j < n; j++) {
-                if (st.count(s[j])) {
-                    break;
-                }
-
-                st.insert(s[j]);
-                ans = max(ans, j - i + 1);
+            while (st.count(s[right])) {
+                st.erase(s[left]);
+                left++;
             }
+
+            st.insert(s[right]);
+
+            ans = max(ans, right - left + 1);
         }
 
         return ans;
