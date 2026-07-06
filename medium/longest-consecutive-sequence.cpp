c944d0b83cdec8518bece176172c7ceac406609b
Saksham Gupta <sakshamofficialwork@gmail.com>
2026-07-06 12:26:51 +0530

    Add Longest Consecutive Sequence [C++]

--- medium/longest-consecutive-sequence.cpp
+++ medium/longest-consecutive-sequence.cpp
@@ -2,32 +2,12 @@
 // Slug    : https://leetcode.com/problems/longest-consecutive-sequence/
 // Lang    : C++
 // Difficulty: Medium
-// Synced  : 2026-07-05T10:10:52.410Z
+// Runtime : 15 ms
+// Memory  : 60 MB
+// Synced  : 2026-07-06T06:56:45.904Z
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-
-        if (nums.empty())
-            return 0;
-
-        sort(nums.begin(), nums.end());
-
-        int longest = 1;
-        int current = 1;
-
-        for (int i = 1; i < nums.size(); i++) {
-
-            if (nums[i] == nums[i - 1])
-                continue;           // Ignore duplicates
-
-            if (nums[i] == nums[i - 1] + 1)
-                current++;          // Extend current sequence
-            else
-                current = 1;        // Start a new sequence
-
-            longest = max(longest, current);
-        }
-
-        return longest;
+        
     }
 };
