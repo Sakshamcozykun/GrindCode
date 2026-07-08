f54f11521f6b7348592c3bb164d934c361aec635
Saksham Gupta <sakshamofficialwork@gmail.com>
2026-07-08 11:08:16 +0530

    Add Delete Node in a Linked List [C++]

--- medium/delete-node-in-a-linked-list.cpp
+++ medium/delete-node-in-a-linked-list.cpp
@@ -2,13 +2,13 @@
 // Slug    : https://leetcode.com/problems/delete-node-in-a-linked-list/
 // Lang    : C++
 // Difficulty: Medium
-// Synced  : 2026-07-08T05:37:16.985Z
+// Synced  : 2026-07-08T05:38:12.935Z
 class Solution {
 public:
     void deleteNode(ListNode* node) {
         node->val = node->next->val;
         ListNode* temp = node->next;
-        node->next = node->next->next;
+        node->next = temp->next;
         delete temp;
     }
 };
