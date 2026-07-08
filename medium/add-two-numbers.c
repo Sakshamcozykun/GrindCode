f7d31d3178b372a6a94173eb2f31617619272e72
Saksham Gupta <sakshamofficialwork@gmail.com>
2026-07-08 11:03:32 +0530

    Add Add Two Numbers [c]

--- medium/add-two-numbers.c
+++ medium/add-two-numbers.c
@@ -4,20 +4,27 @@
 // Difficulty: Medium
 // Runtime : 2 ms
 // Memory  : 77 MB
-// Synced  : 2026-07-08T05:31:43.714Z
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+// Synced  : 2026-07-08T05:33:29.061Z
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
+        ListNode dummy(0);          // Dummy head
+        ListNode* current = &dummy; // Pointer to build result
+        int carry = 0;
+
+        while (l1 != nullptr || l2 != nullptr || carry > 0) {
+            int x = (l1 != nullptr) ? l1->val : 0;
+            int y = (l2 != nullptr) ? l2->val : 0;
+            int sum = x + y + carry;
+
+            carry = sum / 10;
+            current->next = new ListNode(sum % 10);
+            current = current->next;
+
+            if (l1 != nullptr) l1 = l1->next;
+            if (l2 != nullptr) l2 = l2->next;
+        }
+
+        return dummy.next;
     }
 };
