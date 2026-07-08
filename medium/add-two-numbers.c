a2fce1cad516f6f0d56824caff3d4340b3eba844
Saksham Gupta <sakshamofficialwork@gmail.com>
2026-07-08 10:55:29 +0530

    Add Add Two Numbers [c]

---
+++ medium/add-two-numbers.c
@@ -0,0 +1,23 @@
+// Problem : Add Two Numbers
+// Slug    : https://leetcode.com/problems/add-two-numbers/
+// Lang    : c
+// Difficulty: Medium
+// Runtime : 2 ms
+// Memory  : 77 MB
+// Synced  : 2026-07-08T05:25:25.601Z
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        
+    }
+};
