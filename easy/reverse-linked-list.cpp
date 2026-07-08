ff7bc9ea5648f7840cdbb9b872419753a7e42a70
Saksham Gupta <sakshamofficialwork@gmail.com>
2026-07-08 09:36:58 +0530

    Add Reverse Linked List [C++]

--- easy/reverse-linked-list.cpp
+++ easy/reverse-linked-list.cpp
@@ -2,18 +2,18 @@
 // Slug    : https://leetcode.com/problems/reverse-linked-list/
 // Lang    : C++
 // Difficulty: Easy
-// Synced  : 2026-07-08T04:06:03.313Z
-Node* reverse(Node* head) {
-    // Base case
-    if (head == NULL || head->next == NULL)
-        return head;
+// Synced  : 2026-07-08T04:06:55.070Z
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        if (head == NULL || head->next == NULL)
+            return head;
 
-    // Reverse the rest of the list
-    Node* newHead = reverse(head->next);
+        ListNode* newHead = reverseList(head->next);
 
-    // Reverse the current link
-    head->next->next = head;
-    head->next = NULL;
+        head->next->next = head;
+        head->next = NULL;
 
-    return newHead;
-}
+        return newHead;
+    }
+};
