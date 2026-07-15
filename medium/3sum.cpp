c609494edb09f4d797d8c6d2c913f1a17bd28503
Saksham Gupta <sakshamofficialwork@gmail.com>
2026-07-15 11:06:32 +0530

    Add 3Sum [C++]

---
+++ medium/3sum.cpp
@@ -0,0 +1,13 @@
+// Problem : 3Sum
+// Slug    : https://leetcode.com/problems/3sum/
+// Lang    : C++
+// Difficulty: Medium
+// Runtime : 54 ms
+// Memory  : 33.1 MB
+// Synced  : 2026-07-15T05:36:29.494Z
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        
+    }
+};
